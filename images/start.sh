578f3c2ae77e3110223b14d52dde0939b4e3ea43
WilliamYan <34412175+williamyan1024@users.noreply.github.com>
2020-08-06 21:04:42 +0800

    Update start.sh

--- images/start.sh
+++ images/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-nohup /run/brook client -l 127.0.0.1:1080 -i 127.0.0.1 -s 000.000.000.000:00000 -p 411932813 --http &
+nohup /run/brook client -l 127.0.0.1:1080 -i 127.0.0.1 -s 000.000.000.000:00000 -p xxxxxxxxxx --http &
 
 python3.5 /app/app.py
